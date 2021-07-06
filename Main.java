03649f0fb9c0d334a7b194e8828fc38c0b6fa197
beatriz9785 <63080537+beatriz9785@users.noreply.github.com>
2021-07-06 20:54:10 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		NodeStack <Integer> pilha = new NodeStack<>();//criei uma pilha vazia por enquanto
 		
-		//Node<E> n1 = new Node<>();
-		//n1.element = 3;
-		
-		
-		
+		pilha.push(3);
+		pilha.push(4);
+		pilha.push(2);
+		pilha.push(1);
+		pilha.push(5);
+		System.out.println("quantidade de itens na pilha: " + pilha.size);
+		pilha.pop();
+		pilha.pop();
+		System.out.println("agora tiramos dois itens..." + pilha.size);
+		System.out.println("top: " + pilha.top());
 	}
 
 }
