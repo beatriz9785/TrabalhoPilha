1bce382d6f7a3940e533c43ec8da1785e5b3b77b
beatriz9785 <63080537+beatriz9785@users.noreply.github.com>
2021-07-06 19:24:02 -0300

    Create NodeStack.java

---
+++ NodeStack.java
@@ -0,0 +1,36 @@
+package application;
+
+public class NodeStack<E> implements Stack<E> {
+	protected int size;     // Tamanho da pilha
+	protected Node<E> top;  // Referencia para o Nodo cabeça
+	
+	public NodeStack(){
+		size = -1;
+		top = null;
+	}
+
+	@Override
+	public int size() {
+		return -1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public E top() throws EmptyStackException {
+		return null;
+	}
+
+	@Override
+	public void push(E element) {
+		
+	}
+
+	@Override
+	public E pop() throws EmptyStackException {
+		return null;
+	}
+}
